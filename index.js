eb956b79ad50a61bcd172c165ffb90331eb41d22
Leigh Kirk <leigh.kirk@dwp.gsi.gov.uk>
2018-05-16 19:48:07 +0100

    Build out encode function to encode a string

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
 function encode(stringToEncode) {
 
-	return String.fromCharCode((stringToEncode.charCodeAt() + 3));
+	const individualChars = stringToEncode.split('');
+    let convertedCharacter = [];
+
+    for (let i = 0; i < individualChars.length; i++) {
+        convertedCharacter.push(String.fromCharCode((individualChars[i].charCodeAt() + 3)));
+    }
+
+    return convertedCharacter.join('');
 
 };
 
