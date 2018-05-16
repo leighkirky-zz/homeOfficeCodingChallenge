623a18fe6c4d239122d315576f4cf5c2da49853e
Leigh Kirk <leigh.kirk@dwp.gsi.gov.uk>
2018-05-16 21:42:25 +0100

    Add alternative function to show use of an array solution

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ function decode(stringToDecode) {
 
 exports.encode = encode;
 exports.decode = decode;
+
+//Alternative solution using array to hold characters of alphabet
+//Will work with existing unit tests
+
+// const alphabetArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+// function encode(stringToEncode) {
+
+// 	const individualChars = stringToEncode.split('');
+// 	let convertedCharacter = [];
+
+// 	for (let i = 0; i < individualChars.length; i++) {
+//     	convertedCharacter.push((alphabetArray[(alphabetArray.indexOf(individualChars[i]) + 3)]));
+//     }
+
+// 	return convertedCharacter.join('');
+
+// };
