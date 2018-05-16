f124948ee4da78fd5c990338c7a6b9c64a43980c
Leigh Kirk <leigh.kirk@dwp.gsi.gov.uk>
2018-05-16 19:20:18 +0100

    Add test to check encoding of a single character

--- test/test.js
+++ test/test.js
@@ -5,5 +5,9 @@ var codec = require('../index.js');
 describe('encode', function(){
 	it('function should exist', function(){
 		expect(codec.encode).to.exist;
+	}),
+	it('should return character D for input character A', function(){
+		expect(codec.encode('A')).to.equal('D');
 	})
+
 });
